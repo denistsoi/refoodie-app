c671996e073d81f2c442e5b454f47f3d2db2d4a4
Denis Tsoi <denistsoi@gmail.com>
2018-07-07 15:39:59 +0800

    ADD: view -> Resource

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -1,11 +1,42 @@
 <template>
-  <h1>helo </h1>
+  <div>
+    <h1>helo</h1>
+    <div><span>Food: </span><span>{{ items }}</span></div>
+  </div>
 </template>
 
 <script>
+import ApiClient from '../lib/apiClient';
+const api = new ApiClient({ endpoint: 'http://localhost:3000/api/'})
+
+// import 
 export default {
   name: 'Resources',
-  components: {}
+  data: () => {
+    return {
+      items: null
+    }
+  },
+  components: {},
+  methods: {
+    fetchData: async function() {
+      const self = this;
+
+      const food = await api.list('food');
+      console.log(food);
+      this.items = food;
+
+      // fetch('http://localhost:3000/api/food')
+      //   .then(response => response.json())
+      //   .then(data => {
+      //     self.items = data;
+      //   })
+      //   .catch(err => console.log(err));
+    }
+  },
+  mounted() {
+    this.fetchData();
+  }
 }
 </script>
 
--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -1,2 +1,45 @@
 const axios = require('axios');
 
+class ApiClient {
+  constructor(props) {
+    this.endpoint = props.endpoint;
+  }
+
+  list(resource) {
+    console.log(`list ${resource}`);
+    return axios
+      .get(this.endpoint + resource)
+      .then(response => response.data)
+      .catch(err => {
+        console.log(err)
+      });
+  }
+
+  create(resource, data) {
+    console.log(`create ${resource}, ${data}`);
+    return axios  
+      .post(this.endpoint + resource, data) 
+      .then(response => response.data)
+      .catch(err => {
+        console.log(err)
+      });
+  }
+
+  update(resource, data) {
+    console.log(`update ${resource}, ${data}`);
+    return axios
+      .put(this.endpoint + resource, data)
+      .then(response => response.data)
+      .catch(err => {
+        console.log(err)
+      });
+
+  }
+
+  // retrieve(resource) {
+  // }
+  // delete(resource) {
+  // }
+}
+
+export default ApiClient;
