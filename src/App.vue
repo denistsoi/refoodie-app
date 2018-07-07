5a527b3c42e0e77722b87ca8c16c6bc687cc62ba
Denis Tsoi <denistsoi@gmail.com>
2018-07-07 14:32:13 +0800

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img id="logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Refoodie"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+
+#logo {
+  max-width: 300px;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
